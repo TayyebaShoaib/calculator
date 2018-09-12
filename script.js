4435871bd946267ef9609ef4bbf003f7aafa9ace
Tayyeba Shoaib <tayyeba.t@gmail.com>
2018-09-12 21:09:43 +0100

    displaying to screen

--- script.js
+++ script.js
@@ -1,27 +1,10 @@
-const display = document.getElementById("display");
-const operators = ["+", "/", "*"];
-const andMinus = ["-", "+", "/", "*"];
+var calculationDone = false;
+var displayedScript = document.calculator.display.value;
 
-function addToScreen(x, e) {
-    var displayVal = display.value;
-    var lastLetterPlace = displayVal.length - 1;
-    var lastLetter = displayVal[lastLetterPlace];
-
-    if (display.value == "" && operators.includes(x)) {      //The equation should not start from an operator unless minus.
-        console.log("do nothing");
-    } else if (andMinus.includes(x) && x == lastLetter) {    //No two operators should be added consecutively.
-        console.log(lastLetter);
-    } else {
-        display.value += x;
-    }
+function displayToScreen(x) {
+    document.calculator.display.value += x;
 }
 
-function clearScreen() {
-    display.value = "";
-}
-
-function answer() {
-    var equation = display.value;
-    equation = eval(equation);
-    display.value = equation;
+function calculateAnswer() {
+    
 }
