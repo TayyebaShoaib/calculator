2a91dc855d4c913214f4e554e25cb2f47311247d
Tayyeba Shoaib <tayyeba.t@gmail.com>
2018-09-12 21:31:16 +0100

    mostly working

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 var calculationDone = false;
-var displayedScript = document.calculator.display.value;
 
 function displayToScreen(x) {
-    document.calculator.display.value += x;
+    if (!calculationDone) {
+        document.calculator.display.value += x;
+    }
 }
 
 function calculateAnswer() {
-    
+    var equation = document.calculator.display.value;
+    equation = eval(equation);
+    document.calculator.display.value = equation;
+    calculationDone = true;
+}
+
+function clearScreen() {
+    document.calculator.display.value = "";
+    calculationDone = false;
 }
